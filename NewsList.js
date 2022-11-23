f1fcc9f4e837e219c9e44d935dac0d08c3ae012c
bradbelliard <112192112+bradbelliard@users.noreply.github.com>
2022-11-23 10:19:33 -0500

    Update NewsList.js

--- NewsList.js
+++ NewsList.js
@@ -7,7 +7,7 @@ const NewsList = () => {
     
     useEffect(() => {
         const getArticles = async () => {
-            const response = await axios.get('https://newsapi.org/v2/everything?q=Carbon%20Emissions&from=2022-11-11&sortBy=popularity&apiKey=570dea4467c847d4b4f8277f1a7c4e18')
+            const response = await axios.get('https://newsapi.org/v2/everything?q=COVID&from=2022-11-11&sortBy=popularity&apiKey=570dea4467c847d4b4f8277f1a7c4e18')
             console.log(response.data.articles)
             setArticles(response.data.articles)
         }
