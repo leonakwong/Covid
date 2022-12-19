8099eb768a4ab4363ae5131567635407130067d8
bradbelliard <112192112+bradbelliard@users.noreply.github.com>
2022-12-19 15:21:23 -0500

    Update NewsList.js

--- src/NewsList.js
+++ src/NewsList.js
@@ -2,13 +2,21 @@ import React, { useState, useEffect} from 'react'
 import axios from 'axios'
 import NewsItem from './NewsItem'
 
+
+
+let newDate = new Date()
+let date = newDate.getDate();
+let month = newDate.getMonth() + 1;
+let year = newDate.getFullYear();
+    
+
+
 const NewsList = () => {
     const [articles, setArticles] = useState([])
     
     useEffect(() => {
         const getArticles = async () => {
-            const response = await axios.get('https://newsapi.org/v2/top-headlines?q=COVID-19&from=2022-12-19?category=science&apiKey=570dea4467c847d4b4f8277f1a7c4e18'
-            )
+            const response = await axios.get('https://newsapi.org/v2/top-headlines?q=COVID-19&from=' + year + '-' + month + '-' + date + '?category=science&apiKey=570dea4467c847d4b4f8277f1a7c4e18')
             console.log(response.data.articles)
             setArticles(response.data.articles)
         }
