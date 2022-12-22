32a8eacb86e4c7abd03ed06108918b3c8ba050de
leonakwong <66903627+leonakwong@users.noreply.github.com>
2022-12-22 16:01:15 -0500

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -21,21 +21,107 @@ app.get("/hospital", (req, res) => {
   .catch(error => {
     res.status(500).send(error);
   })
-  // loca.getVaxCenters()
-  // .then(response => {
-  //   res.status(200).send(response);
-  // })
-  // .catch(error => {
-  //   res.status(500).send(error);
-  // })
-  // loca.getTestCenters()
-  // .then(response => {
-  //   res.status(200).send(response);
-  // })
-  // .catch(error => {
-  //   res.status(500).send(error);
-  // })
 });
+
+app.get("/rapid", (req, res) => {
+  loca.getTestCenters()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+});
+
+app.get("/vax", (req, res) => {
+  loca.getVaxCenters()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+});
+
+app.get('/moderna', (req, res) => {
+  loca.getModerna()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.get('/pfizer', (req, res) => {
+  loca.getPfizer()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.get('/janssen', (req, res) => {
+  loca.getJanssen()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.get('/novavax', (req, res) => {
+  loca.getNovavax()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.get('/modernabivalent', (req, res) => {
+  loca.getModernaBiva()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.get('/pfizerbivalent', (req, res) => {
+  loca.getPfizerBiva()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.get('/express', (req, res) => {
+  loca.getExpress()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
+
+app.get('/mobiletest', (req, res) => {
+  loca.getMobileTest()
+  .then(response => {
+    res.status(200).send(response);
+  })
+  .catch(error => {
+    res.status(500).send(error);
+  })
+})
 
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- backend/test.js
+++ backend/test.js
@@ -21,7 +21,7 @@ const getHospitals = () => {
 
 const getVaxCenters = () => {
     return new Promise(function(resolve, reject) {
-      pool.query('SELECT * FROM vaxcenter ORDER BY id ASC', (error, results) => {
+      pool.query('SELECT DISTINCT loc_name, loc_admin_street, loc_phone, loc_admin_city, loc_admin_state, loc_admin_zip, web_address, latitude, longitude FROM vaxcenter ORDER BY loc_name', (error, results) => {
         if (error) {
           reject(error)
         }
@@ -41,10 +41,106 @@ const getTestCenters = () => {
     }) 
 }
 
+const getModerna = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query("SELECT DISTINCT loc_name, loc_admin_street, loc_phone, loc_admin_city, loc_admin_state, loc_admin_zip, web_address, latitude, longitude FROM vaxcenter WHERE med_name NOT LIKE '%Bivalent%' AND med_name LIKE '%Moderna%'", (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+
+const getPfizer = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query("SELECT DISTINCT loc_name, loc_admin_street, loc_phone, loc_admin_city, loc_admin_state, loc_admin_zip, web_address, latitude, longitude FROM vaxcenter WHERE med_name NOT LIKE '%Bivalent%' AND med_name LIKE '%Pfizer%'", (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+
+const getJanssen = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query("SELECT DISTINCT loc_name, loc_admin_street, loc_phone, loc_admin_city, loc_admin_state, loc_admin_zip, web_address, latitude, longitude FROM vaxcenter WHERE med_name NOT LIKE '%Bivalent%' AND med_name LIKE '%Janssen%'", (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+
+const getNovavax = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query("SELECT DISTINCT loc_name, loc_admin_street, loc_phone, loc_admin_city, loc_admin_state, loc_admin_zip, web_address, latitude, longitude FROM vaxcenter WHERE med_name NOT LIKE '%Bivalent%' AND med_name LIKE '%Novavax%'", (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+
+const getModernaBiva = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query("SELECT DISTINCT loc_name, loc_admin_street, loc_phone, loc_admin_city, loc_admin_state, loc_admin_zip, web_address, latitude, longitude FROM vaxcenter WHERE med_name LIKE '%Bivalent%' AND med_name LIKE '%Moderna%'", (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+
+const getPfizerBiva = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query("SELECT DISTINCT loc_name, loc_admin_street, loc_phone, loc_admin_city, loc_admin_state, loc_admin_zip, web_address, latitude, longitude FROM vaxcenter WHERE med_name LIKE '%Bivalent%' AND med_name LIKE '%Pfizer%'", (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+
+const getExpress = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query("SELECT * FROM mobiletest WHERE type LIKE '%Express%' ORDER BY id ASC", (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+
+const getMobileTest = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query("SELECT * FROM mobiletest WHERE type LIKE '%Mobile%' ORDER BY id ASC", (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+
 module.exports = {
     getHospitals,
     getTestCenters,
-    getVaxCenters
+    getVaxCenters,
+    getModerna,
+    getPfizer,
+    getJanssen,
+    getNovavax,
+    getPfizerBiva,
+    getModernaBiva,
+    getExpress,
+    getMobileTest,
 };
 
 //import db by -> const pool = require("./db");
